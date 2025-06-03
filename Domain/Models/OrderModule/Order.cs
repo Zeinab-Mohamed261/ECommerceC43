2147defb895124b465dd11bea01d776be9ecde2d
PC <PC@DESKTOP-4QNGLLJ>
2025-06-04 02:14:57 +0300

    Order Modules Entity

---
+++ Domain/Models/OrderModule/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.OrderModule
+{
+    public class Order:BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderStatus OrderStatus { get; set; }
+        public OrderAddress Address { get; set; } = default!;
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+        public ICollection<OrderItem> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        //[NotMapped] //not mapped in db
+        //public decimal Total { get; set; }
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Price;
+        
+    }
+}
