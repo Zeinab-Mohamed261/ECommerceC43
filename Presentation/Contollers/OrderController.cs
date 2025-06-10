43573467bb8bb83218870acc0ab99bfded6c08f9
PC <PC@DESKTOP-4QNGLLJ>
2025-06-10 16:32:52 +0300

    Order controller [Get All Delivery Method ,Get All Orders,Get Order By Id]

--- Presentation/Contollers/OrderController.cs
+++ Presentation/Contollers/OrderController.cs
@@ -13,7 +13,7 @@ namespace Presentation.Contollers
 {
     [ApiController]
     [Route("api/[Controller]")] //baseUrl/api/Order
-    public class OrderController(IServiceManager _serviceManager) :ControllerBase
+    public class OrderController(IServiceManager _serviceManager) : ControllerBase
     {
         //Create Order
         [Authorize]
@@ -22,6 +22,31 @@ namespace Presentation.Contollers
         {
             var email = User.FindFirstValue(ClaimTypes.Email);
             var order = await _serviceManager.OrderService.CreateOrder(orderDto, email);
+            return Ok(order);
+        }
+        //Get Delivery Methods
+        [HttpGet("DeliveryMethods")]
+        public async Task<ActionResult<IEnumerable<DeliveryMethodsDto>>> GetDeliveryMethodsAsync()
+        {
+            var deliveryMethods = await _serviceManager.OrderService.GetDeliveryMethodAsync();
+            return Ok(deliveryMethods);
+        }
+
+        //Get All Orders
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetAllOrders()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var orders = await _serviceManager.OrderService.GetAllOrdersAsync(email);
+            return Ok(orders);
+        }
+
+        //Get Order By Id
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdAsync(Guid id)
+        {
+            var order =await _serviceManager.OrderService.GetOrderByIdAsync(id);
             return Ok(order);
         }
     }
